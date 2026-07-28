4077e0b9f8950dd40f888c55234b338bc3a5ed50
vijaysinara <123311408+vijaysinara@users.noreply.github.com>
2026-07-28 14:26:01 +0530

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require("../../lib/auth");
-
-module.exports = async (req, res) => {
-  res.setHeader("Set-Cookie", clearSessionCookie());
-  res.json({ ok: true });
-};
--- api/auth/session.js
+++
@@ -1,5 +0,0 @@
-const { isAuthed } = require("../../lib/auth");
-
-module.exports = async (req, res) => {
-  res.json({ loggedIn: isAuthed(req) });
-};
